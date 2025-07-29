7ddc70b6b0ba2239d05d56d7e56ccf6256958b84
Luis Toscano <luismariotoscanopalomino@gmail.com>
2025-07-29 06:54:49 -0500

    Fin bugs and contact section
    It´s still missing the email service

--- portfolio/src/components/HeroSection.jsx
+++ portfolio/src/components/HeroSection.jsx
@@ -33,7 +33,7 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-0.5 flex flex-col items-center animate-bounce">
+      <div className="absolute bottom-8  transform -translate-x-0.5 flex flex-col items-center animate-bounce">
         <span className="text-sm text-muted-foreground mb-2">Scroll</span>
         <ArrowDown className="size-5 text-primary" />
       </div>
